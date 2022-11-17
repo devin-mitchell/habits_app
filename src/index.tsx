b059c6174d71dd8520d9726cc10065c9a2cbfdf9
Devin Mitchell <devin.a.mitchell@gmail.com>
2022-11-17 10:29:12 -0700

    add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import {ChakraProvider} from '@chakra-ui/react'
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  <ApolloProvider client={client}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
